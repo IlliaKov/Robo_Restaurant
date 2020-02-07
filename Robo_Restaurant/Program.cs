b3becc8409325cc327c0ac5edf20fc26bc10dfad
Illia Kovalenko <IlliaKov@users.noreply.github.com>
2020-02-07 20:07:51 +0200

    First commit

---
+++ Robo_Restaurant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robo_Restaurant
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
